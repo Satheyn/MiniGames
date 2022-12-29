2da944be45e9a15f81688de91886df60af412054
Олександр <115733936+SSanok-UA@users.noreply.github.com>
2022-12-29 09:36:44 +0200

    Add files via upload

---
+++ Menu.gd
@@ -0,0 +1,8 @@
+extends Control
+
+func _on_Button_pressed():
+	get_tree().change_scene("res://dodge_the_creeps/Main.tscn")
+
+
+func _on_Button2_pressed():
+	get_tree().change_scene("res://Cubio/world.tscn")
